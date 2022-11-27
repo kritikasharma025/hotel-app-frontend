10af41baca75043678d0d09ecaaf8b423c38e54a
Kritika Sharma <kritika8815@gmail.com>
2022-11-27 22:58:48 +0530

    Added New Hotel Page

--- authFunctions/checkLogin.js
+++ authFunctions/checkLogin.js
@@ -1,13 +1,15 @@
-var appUrl = "https://kritikasharma025.github.io/hotel-app-frontend"
-// var appUrl = "http://localhost:5500";
-// var serverUrl = "http://localhost:8080/api"
-var serverUrl = "https://hotel-app-backend.onrender.com/api"
+// var appUrl = "https://kritikasharma025.github.io/hotel-app-frontend"
+// // var appUrl = "http://localhost:5500";
+// // var serverUrl = "http://localhost:8080/api"
+// var serverUrl = "https://hotel-app-backend.onrender.com/api"
 
-window.onload = function () {
-    if(localStorage.getItem("token")){
-        // const authenticateToken - await fetch()
-    }
-    else{
-        // window.location.href = `${appUrl}/login.html`
-    }
-}
+// window.onload = function () {
+//     if(localStorage.getItem("jwToken")){
+//         // const authenticateToken - await fetch()
+//         return window.location.href = `${appUrl}/index.html`
+//     }
+//     else{
+//         if (window.location.href !== `${appUrl}/login.html`) return window.location.href = `${appUrl}/login.html`
+//         return
+//     }
+// }
